658ba174dca63395b2fd3596dfaee9810dbc6404
Rhett <rhett.burton7@gmail.com>
2018-12-04 15:07:43 -0700

    created test model and orderassaytest model

---
+++ INTEX/Models/Order_Assay_Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.Models
+{
+    [Table("Order_Assay_Test")]
+    public class Order_Assay_Test
+    {
+        [DisplayName("Order Number")]
+        public int WorkOrderNumber { get; set; }
+
+        
+        public int AssayID { get; set; }
+
+        
+        public int TestID { get; set; }
+
+        
+        public int EmployeeID { get; set; }
+
+        [DisplayName("Complete?")]
+        public bool? IsComplete  { get; set; }
+
+        [DisplayName("Total Cost")]
+        public double TotalCost { get; set; }
+
+        
+        public int ResultID { get; set; }
+
+    }
+}
---
+++ INTEX/Models/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.Models
+{
+    [Table("Test")]
+    public class Test
+    {
+        [Key]
+        public int TestID { get; set; }
+
+        public string TestName { get; set; }
+        public double BaseCost { get; set; }
+
+
+    }
+}
